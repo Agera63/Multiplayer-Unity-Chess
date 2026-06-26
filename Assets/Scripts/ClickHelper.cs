62d86675296cf5cf0d7207bbe3e693a3f3f6f07a
DESKTOP-JNGH4DF\Eric <ericz2016@gmail.com>
2026-06-26 11:38:17 -0400

    refactor: added XML doc comments to ClickHelper file

--- Assets/Scripts/ClickHelper.cs
+++ Assets/Scripts/ClickHelper.cs
@@ -3,6 +3,10 @@ using UnityEngine.InputSystem;
 
 public class ClickHelper : MonoBehaviour
 {
+    /// <summary>
+    /// Allows for a user click to be inputed and returns a the clicked gameobject.
+    /// </summary>
+    /// <returns>The game object that was clicked.</returns>
     public static GameObject FindClickedObject()
     {
         Vector2 mousePosition = Mouse.current.position.ReadValue();
